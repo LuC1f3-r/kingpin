be2dad86e01b0f311596af1042f94747a2cbe7cd
Niyaz Ahamad Herkal <niyaz47nhh@gmail.com>
2025-07-25 00:10:09 +0530

    changed coming soon page

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "add5c275b8e18429"; }
+/******/ 		__webpack_require__.h = function() { return "25157ba3d5c063f8"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
